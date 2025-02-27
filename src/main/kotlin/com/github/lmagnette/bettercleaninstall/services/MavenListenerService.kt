769d61ca10810aa1b7a80fb2ff5db67c3c9852fc
Loic Magnette <loic.magnette@gmail.com>
2025-02-27 13:35:37 +0100

    chore(clean up): remove some left over from the template

--- src/main/kotlin/com/github/lmagnette/bettercleaninstall/services/MavenListenerService.kt
+++ src/main/kotlin/com/github/lmagnette/bettercleaninstall/services/MavenListenerService.kt
@@ -2,9 +2,6 @@ package com.github.lmagnette.bettercleaninstall.services
 
 import com.github.lmagnette.bettercleaninstall.util.HttpClient
 import com.github.lmagnette.bettercleaninstall.views.ImageDialogWrapper
-import com.intellij.notification.Notification
-import com.intellij.notification.NotificationType
-import com.intellij.notification.Notifications
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -22,18 +19,5 @@ class MavenListenerService {
 
         val dialog = ImageDialogWrapper(selectedImg);
         dialog.show()
-
-        // This is where you implement your custom action
-        // For example, show a notification
-        val notification = Notification(
-            "com.github.lmagnette.bettercleaninstall",
-            "Maven Clean Install Detected",
-            imgUrls.toString(),
-            NotificationType.INFORMATION
-        )
-        Notifications.Bus.notify(notification, project)
-
-        // Add your custom logic here
-        // For example: run a script, update files, etc.
     }
 }
